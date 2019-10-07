910464aec0a69a501afad25537fefa5bb46ccf93
Vitaly <vitys96@mail.ru>
2019-10-07 21:55:51 +0300

    Initial Commit

---
+++ Tinder_Analog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder_Analog
+//
+//  Created by Vitaly on 07/10/2019.
+//  Copyright © 2019 Vitaly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
